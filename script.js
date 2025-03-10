7406586e1ecc55e93fbcf99f4bd8a5be70300d60
lulelaboratory <145885543+lulelaboratory@users.noreply.github.com>
2025-03-10 23:46:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Smooth scrolling for internal links
+// Smooth scrolling for internal navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+  anchor.addEventListener('click', function(e) {
     e.preventDefault();
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
